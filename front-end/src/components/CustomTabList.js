33afc16fc84b327844908b0945b271b299be286e
Ranko <ranko746.dev@gmail.com>
2023-04-27 02:47:27 +0900

    Added NoItem view.

--- front-end/src/components/CustomTabList.js
+++ front-end/src/components/CustomTabList.js
@@ -5,33 +5,56 @@ import { ExpandLess, ExpandMore } from '@mui/icons-material';
 const CustomTabList = (props) => {
   const {
     answers,
-    detailId,
+    detailAnswerId,
     tabId,
     handleCollapseChange
   } = props;
 
+  let itemExist = false;
+
   return (
     <List>
-      {answers.map((row, index) => (
-        row.status === tabId ?
-          <div key={index}>
-            <ListItemButton onClick={ () => handleCollapseChange(row.id)}>
-              <ListItemText primary={ row.title } />
-              {detailId === row.id ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={ detailId === row.id } timeout="auto" unmountOnExit>
-              <List>
-                <ListItem sx={{ pl: 4, display: "block" }}>
-                  <ListItemText primary={ row.description } />
-                  <ListItemText primary={ row.link } sx={{ wordWrap: "break-word" }}/>
-                </ListItem>
-              </List>
-            </Collapse>
-            <Divider />
-          </div>
-        :
-          <div key={index}></div>
-      ))}
+        <div>
+          {
+            answers.map((row, index) => {
+              if (row.status === tabId) {
+                itemExist = true;
+                return (
+                  <div key={index}>
+                    <ListItemButton onClick={ () => handleCollapseChange(row.id)}>
+                      <ListItemText primary={ row.title } />
+                      { detailAnswerId === row.id ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <Collapse in={ detailAnswerId === row.id } timeout="auto" unmountOnExit>
+                      <List>
+                        <ListItem sx={{ pl: 4, display: "block" }}>
+                          <ListItemText primary={ row.description } />
+                          <ListItemText primary={ row.link } sx={{ wordWrap: "break-word" }}/>
+                        </ListItem>
+                      </List>
+                    </Collapse>
+                    <Divider />
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={index}></div>
+                );
+              }
+            }
+          )}
+
+          {
+            !itemExist
+            ?
+            <ListItem>
+              <ListItemText primary="No Answers" />
+            </ListItem>
+            :
+            <></>
+          }
+          
+        </div>
     </List>
   );
 }
